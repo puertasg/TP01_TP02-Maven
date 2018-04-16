5b006eb28a41865a0df1aed0a0e1484fea19da8c
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-16 11:27:22 +0200

    Ajout librairie jfiglet

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        String asciiArt = FigletFont.convertOneLine("App");
+	    System.out.println(asciiArt);
     }
 }
